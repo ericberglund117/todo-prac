bbd62c630b6f0617477c5b2c4f7cfae4f99e7a6a
Eric Berglund <ericberglund117@gmail.com>
2023-03-13 15:26:30 -0600

    Create remove task feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,26 @@ export class AppComponent {
     'Visit Ann',
     'Call Dad',
     'Go to the gym',
-    'Wash the dishes', 
+    'Wash the dishes',
     'Shop for the party'
   ];
 
   add = (newTask: string) => {
     this.tasks.push(newTask)
+  }
+
+  remove = (existingTask: string) => {
+   let confirmation = confirm(`Are you sure you want to remove the following task? \n "${existingTask}"`)
+   if (confirmation) this.tasks = this.tasks.filter(task => task !== existingTask)
+  }
+
+  done = (finishedTask: string) => {
+    alert(`the task ${finishedTask} is done`)
+  }
+}
+
+class Task {
+  constructor(public title: string) {
+
   }
 }
