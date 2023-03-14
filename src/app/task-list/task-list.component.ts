c729c071d05b39b40b3aa8d2e19f82bdd03a5069
Eric Berglund <ericberglund117@gmail.com>
2023-03-14 13:40:36 -0600

    Rearrange components

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
+  tasks: Task[] = [
+    new Task('Visit Ann'),
+    new Task('Call Dad'),
+    new Task('Go to the gym'),
+    new Task('Wash the dishes'),
+    new Task('Shop for the party')
+   ];
 
-}
+   add = (newTask: string) => {
+     this.tasks.push(new Task (newTask))
+   }
+
+   remove = (existingTask: Task) => {
+    let confirmation = confirm(`Are you sure you want to remove the following task? \n "${existingTask.title}"`)
+    if (confirmation) this.tasks = this.tasks.filter(task => task !== existingTask)
+   }
+ }
+
+ class Task {
+   constructor(public title: string) {
+
+   }
+
+   toggleIsDone = () => {
+     this.isDone = !this.isDone
+   }
+
+   public isDone = false;
+ }
+
