f6b4fdef3cb7be3e461a22c5ef4b0acd3a1da21e
Eric Berglund <ericberglund117@gmail.com>
2023-03-15 13:54:56 -0600

    Create tasks link to new page

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -4,6 +4,7 @@ import { CalendarComponent } from './calendar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     CommonModule,
     BrowserAnimationsModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    RouterModule
   ]
 })
 export class CalendarModule { }
