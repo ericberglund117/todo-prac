4cbe7b3dd460661967069b2de897a896093e3b84
Eric Berglund <ericberglund117@gmail.com>
2023-03-15 14:03:50 -0600

    Get date from route

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {
+    let date: Date =  new Date(this.route.snapshot.params['date']);
+    console.log(date)
+  }
+
+  ngOnInit(): void {
+
+  }
   tasks: Task[] = [
     new Task('Visit Ann'),
     new Task('Call Dad'),
